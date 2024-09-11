bbe45d7452056d67cce00a3faae6f5b746726846
Taka005 <01taka005@gmail.com>
2024-09-11 09:04:59 +0900

    change:API構築

--- Server/src/Server.ts
+++ Server/src/Server.ts
@@ -1,13 +1,15 @@
-import express from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import Log from "./Log"
 import Config from "./Config";
 import Utils from "./Utils";
 
 class Server{
-  public app: express.Express;
+  public app: Express;
 
   constructor(){
     this.app = express();
+
+    this.route();
   }
 
   /**
@@ -25,8 +27,35 @@ class Server{
     });
   }
 
-  route(): void{
+  /**
+   * APIの制御
+   */
+  private route(): void{
+    this.app.use((req: Request,res: Response,next: NextFunction)=>{
+      Log.info(`${req.ip} - [${req.method}] ${req.originalUrl}`);
 
+      if(this.authorization(req.headers["authorization"])){
+        next();
+      }else{
+        res.status(401).json({
+          message: "認証されていません"
+        });
+      }
+    });
+
+
+  }
+
+  /**
+   * APIKeyを確認します
+   * Config.isUseApiKeyが無効の場合は常に真が返されます
+   */
+  private authorization(authHeader: string | undefined): boolean{
+    if(!Config.isUseApiKey) return true;
+
+    if(!authHeader||!authHeader.startsWith("Bearer ")) return false;
+
+    return authHeader.split(" ")[1] === this.getApiKey();
   }
 
   /**
@@ -35,16 +64,6 @@ class Server{
    */
   public getApiKey(): string{
     return Utils.toHash(Config.token);
-  }
-
-  /**
-   * APIKeyを確認します
-   * Config.isUseApiKeyが偽の場合は常に真が返されます
-   */
-  public checkApiKey(value: string): boolean{
-    if(!Config.isUseApiKey) return true;
-
-    return value === this.getApiKey();
   }
 }
 
