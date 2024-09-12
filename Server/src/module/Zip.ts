93400fe878791a5c075f09b5aa6c16a52be40e1c
Taka005 <01taka005@gmail.com>
2024-09-12 10:42:47 +0900

    change:調整

--- Server/src/module/Zip.ts
+++ Server/src/module/Zip.ts
@@ -1,23 +1,45 @@
+import path from "path";
 import yauzl from "yauzl";
 
+/**
+ * ファイルのエントリーデータ
+ */
+type Entry = {
+  entryName: string
+  name: string
+  size: number
+  time: string
+}
+
 class Zip{
+  /**
+   * ファイルのパス
+   */
   public readonly path: string;
 
-  constructor(path: string){
-    this.path = path;
+  constructor(filePath: string){
+    this.path = filePath;
   }
 
-  getEntries(){
+  /**
+   * 全てのエントリーを取得
+   */
+  getEntries(): Promise<Entry[]>{
     return new Promise((resolve,reject)=>{
-      yauzl.open(this.zipPath,{ lazyEntries: true },(err,zipfile)=>{
+      yauzl.open(this.zipPath,{ lazyEntrites: true },(err,zipfile)=>{
         if(err) return reject(err);
 
-        const entries = [];
+        const entries: Entry[] = [];
         zipfile.readEntry();
 
         zipfile.on("entry",(entry)=>{
           if(!entry.fileName.endsWith("/")){
-            entries.push(entry.fileName);
+            entries.push({
+              entryName: entry.fileName,
+              name: path.basename(entry.fileName),
+              size: entry.uncompressedSize,
+              time: entry.lastModFileTime
+            });
           }
 
           zipfile.readEntry();
@@ -34,7 +56,10 @@ class Zip{
     });
   }
 
-  getData(fileName): Promise<Buffer>{
+  /**
+   * ファイルのバッファを取得
+   */
+  getData(fileName: string): Promise<Buffer>{
     return new Promise<Buffer>((resolve,reject)=>{
       yauzl.open(this.zipPath,{ lazyEntries: true },(err,zipfile)=>{
         if(err) return reject(err);
@@ -46,7 +71,7 @@ class Zip{
             zipfile.openReadStream(entry,(err,readStream)=>{
               if(err) return reject(err);
 
-              const chunks = [];
+              const chunks: string = [];
 
               readStream.on("data",(chunk)=>chunks.push(chunk));
 
