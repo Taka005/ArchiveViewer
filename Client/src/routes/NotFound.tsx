3210629b79ffde6a176b41a11919e07cb3a4c5de
Taka005 <01taka005@gmail.com>
2024-09-14 21:24:13 +0900

    fix:ミス

--- Client/src/routes/NotFound.tsx
+++ Client/src/routes/NotFound.tsx
@@ -1,10 +1,10 @@
 const NotFound = ()=>{
   return (
-    <div class="bg-gray-100">
-      <div class="h-screen flex flex-col justify-center items-center">
-        <h1 class="text-8xl font-bold text-gray-800">404</h1>
-        <p class="text-4xl font-medium text-gray-800">ページが見つかりません</p>
-        <a href="/" class="mt-4 text-xl text-blue-600 hover:underline">アーカイブ一覧に戻る</a>
+    <div className="bg-gray-100">
+      <div className="h-screen flex flex-col justify-center items-center">
+        <h1 className="text-8xl font-bold text-gray-800">404</h1>
+        <p className="text-4xl font-medium text-gray-800">ページが見つかりません</p>
+        <a href="/" className="mt-4 text-xl text-blue-600 hover:underline">アーカイブ一覧に戻る</a>
       </div>
     </div>
   )
