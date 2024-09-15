35df26b572d2f87378d73efb22a498f2de98961a
Taka005 <01taka005@gmail.com>
2024-09-15 09:11:55 +0900

    change:ミス

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -8,9 +8,8 @@ import NotFound from "./routes/NotFound";
 const App = ()=>{
   return (
     <Routes>
-      <Route path="/" element={ <Archive /> } >
-        <Route path=":seriesId" element={ <Series /> } />
-      </Route>
+      <Route path="/" element={ <Archive /> } />
+      <Route path="/archive/:seriesId" element={ <Series /> } />
       <Route path="/viewer/:bookId" element={ <Viewer /> } />
       <Route path="/setting" element={ <Setting /> } />
       <Route path="*" element={ <NotFound /> } />
