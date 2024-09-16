ecdc7564cf5a11cca9bf2d5e906c58dd7709c926
Taka005 <01taka005@gmail.com>
2024-09-16 09:52:51 +0900

    change:test

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { HeaderProps } from "../@types";
 
 const Header = (props: HeaderProps)=>{
   return (
-    <div className="w-100 h-20 flex items-center justify-center">
+    <div className="w-100 h-20 flex items-center justify-center flex-col">
       {props.children}
     </div>
   )
